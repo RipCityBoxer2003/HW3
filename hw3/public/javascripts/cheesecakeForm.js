88643c823fbcb0af10832083a772ed0cdf4d8576
RipCityBoxer2003 <francos25@up.edu>
2023-02-10 15:53:42 -0800

    First Fix of GITHUB ISSUES
    (Spanned for hours unfortunately)

---
+++ hw3/public/javascripts/cheesecakeForm.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function()
+{
+  var quantity = 1;
+
+  $("#submitBtn").on("click", function(){
+    var text = $("#specialInstructions").val();
+    // If Vegan is written
+    if(text.toLowerCase().includes("vegan"))
+    {
+      alert("Warning: Cheesecakes Contain Dairy");
+    }
+    // else = Hide Form, Read Data, and Write Message
+    else
+    {
+      $("#outer").hide();
+      var topping = $('input[name="Flavors"]:checked').val();
+      var notes = $("#specialInstructions").val();
+      $("#nameChange").text("Thank you! Your order of " +  quantity + " " + topping + " Cheesecake(s) has been placed. Notes:" + notes);
+    }
+});
+
+
+$("#numOfCake").change(function(){
+        quantity = $(this).val();
+  });
+
+
+});
+
---
+++ hw3/public/javascripts/monthChanger.js
@@ -0,0 +1,41 @@
+$(document).ready(function()
+{
+
+$("#Jan").click(function(){
+  $(".dropbtn").text("Jan");
+});
+$("#Feb").click(function(){
+  $(".dropbtn").text("Feb");
+});
+$("#Mar").click(function(){
+  $(".dropbtn").text("Mar");
+});
+$("#Apr").click(function(){
+  $(".dropbtn").text("Apr");
+});
+$("#May").click(function(){
+  $(".dropbtn").text("May");
+});
+$("#Jun").click(function(){
+  $(".dropbtn").text("Jun");
+});
+$("#Jul").click(function(){
+  $(".dropbtn").text("Jul");
+});
+$("#Aug").click(function(){
+  $(".dropbtn").text("Aug");
+});
+$("#Sep").click(function(){
+  $(".dropbtn").text("Sep");
+});
+$("#Oct").click(function(){
+  $(".dropbtn").text("Oct");
+});
+$("#Nov").click(function(){
+  $(".dropbtn").text("Nov");
+});
+$("#Dec").click(function(){
+  $(".dropbtn").text("Dec");
+});
+
+});
